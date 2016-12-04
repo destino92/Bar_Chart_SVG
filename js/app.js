7cbee57aed073a0abdc28ae28192ac29aded4374
destino92 <destinodello@gmail.com>
2016-12-04 23:57:24 +0300

    chore: added new data for generating chart

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 var width = 960,
     height = 500;
 
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
-
 var y = d3.scale.linear()
     .range([height, 0]);
 
@@ -11,25 +8,26 @@ var chart = d3.select(".chart")
     .attr("width", width)
     .attr("height", height);
 
-d3.csv("data.csv", type, function(error, data) {
-  x.domain(data.map(function(d) { return d.name; }));
-  y.domain([0, d3.max(data, function(d) {return d.value;})]);
+d3.csv("data1.csv", type, function(error, data) {
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+  var barWidth = width / data.length;
 
   var bar = chart.selectAll("g")
       .data(data)
     .enter().append("g")
-      .attr("transform", function(d) { return "translate(" + x(d.name) + ",0)"; });
+      .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
 
   bar.append("rect")
       .attr("y", function(d) { return y(d.value); })
       .attr("height", function(d) { return height - y(d.value); })
-      .attr("width", x.rangeBand());
+      .attr("width", barWidth - 1);
 
   bar.append("text")
-      .attr("x", x.rangeBand() / 2)
+      .attr("x", barWidth / 2)
       .attr("y", function(d) { return y(d.value) + 3; })
       .attr("dy", ".75em")
-      .text(function(d) {return d.value; });
+      .text(function(d) { return d.value; });
 });
 
 function type(d) {
